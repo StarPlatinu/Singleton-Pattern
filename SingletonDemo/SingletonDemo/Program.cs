5c9383763ec65aec6cafb26fdcbad4614f17b508
StarPlatinu <thanhvp1509@gmail.com>
2023-05-25 00:47:16 +0700

    Singleton Demo

---
+++ SingletonDemo/SingletonDemo/Program.cs
@@ -0,0 +1,220 @@
+﻿using System;
+
+namespace Singleton.Structural
+{
+    public sealed class Singleton
+    {
+        // The Singleton's constructor should always be private to prevent
+        // direct construction calls with the `new` operator.
+        private Singleton() { }
+
+        // The Singleton's instance is stored in a static field. There there are
+        // multiple ways to initialize this field, all of them have various pros
+        // and cons. In this example we'll show the simplest of these ways,
+        // which, however, doesn't work really well in multithreaded program.
+        private static Singleton _instance;
+
+        // This is the static method that controls the access to the singleton
+        // instance. On the first run, it creates a singleton object and places
+        // it into the static field. On subsequent runs, it returns the client
+        // existing object stored in the static field.
+        public static Singleton GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new Singleton();
+            }
+            return _instance;
+        }
+
+        // Finally, any singleton should define some business logic, which can
+        // be executed on its instance.
+        public void someBusinessLogic()
+        {
+            // ...
+        }
+    }
+
+    //Thread-safe Singleton
+
+    // This Singleton implementation is called "double check lock". It is safe
+    // in multithreaded environment and provides lazy initialization for the
+    // Singleton object.
+    class Singleton1
+    {
+        private Singleton1() { }
+
+        private static Singleton1 _instance;
+
+        // We now have a lock object that will be used to synchronize threads
+        // during first access to the Singleton.
+        private static readonly object _lock = new object();
+
+        public static Singleton1 GetInstance(string value)
+        {
+            // This conditional is needed to prevent threads stumbling over the
+            // lock once the instance is ready.
+            if (_instance == null)
+            {
+                // Now, imagine that the program has just been launched. Since
+                // there's no Singleton instance yet, multiple threads can
+                // simultaneously pass the previous conditional and reach this
+                // point almost at the same time. The first of them will acquire
+                // lock and will proceed further, while the rest will wait here.
+                lock (_lock)
+                {
+                    // The first thread to acquire the lock, reaches this
+                    // conditional, goes inside and creates the Singleton
+                    // instance. Once it leaves the lock block, a thread that
+                    // might have been waiting for the lock release may then
+                    // enter this section. But since the Singleton field is
+                    // already initialized, the thread won't create a new
+                    // object.
+                    if (_instance == null)
+                    {
+                        _instance = new Singleton1();
+                        _instance.Value = value;
+                    }
+                }
+            }
+            return _instance;
+        }
+
+        // We'll use this property to prove that our Singleton really works.
+        public string Value { get; set; }
+    }
+
+    class LoadBalancer
+    {
+        private static LoadBalancer _instance;
+        private List<string> _servers = new List<string>();
+        private Random _random = new Random();
+        // Lock synchronization object
+        private static object syncLock = new object();
+        // Constructor (protected)
+        protected LoadBalancer()
+        {
+            // List of available servers
+            _servers.Add("ServerI");
+            _servers.Add("ServerII");
+            _servers.Add("ServerIII");
+            _servers.Add("ServerIV");
+            _servers.Add("ServerV");
+        }
+        public static LoadBalancer GetLoadBalancer()
+        {
+            // Support multithreaded applications through
+            // 'Double checked locking' pattern which (once the instance exists) avoids locking each time the method is invoked
+            if (_instance == null)
+            {
+                lock (syncLock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new LoadBalancer();
+                    }
+                }
+            }
+            return _instance;
+        }
+        // Simple, but effective random load balancer
+        public string Server
+        {
+            get
+            {
+                int r = _random.Next(_servers.Count);
+                return _servers[r].ToString();
+            }
+        }
+    }
+
+    class Program {
+        //
+        static void NaiveSingleton()
+        {
+            // The client code.
+            Singleton s1 = Singleton.GetInstance();
+            Singleton s2 = Singleton.GetInstance();
+
+            if (s1 == s2)
+            {
+                Console.WriteLine("Singleton works, both variables contain the same instance.");
+            }
+            else
+            {
+                Console.WriteLine("Singleton failed, variables contain different instances.");
+            }
+        }
+        //
+
+        static void TestSingleton(string value)
+        {
+            Singleton1 singleton = Singleton1.GetInstance(value);
+            Console.WriteLine(singleton.Value);
+        }
+        static void ThreadSafeSingleton()
+        {
+            // The client code.
+
+            Console.WriteLine(
+                "{0}\n{1}\n\n{2}\n",
+                "If you see the same value, then singleton was reused (yay!)",
+                "If you see different values, then 2 singletons were created (booo!!)",
+                "RESULT:"
+            );
+
+            Thread process1 = new Thread(() =>
+            {
+                TestSingleton("FOO");
+            });
+            Thread process2 = new Thread(() =>
+            {
+                TestSingleton("BAR");
+            });
+
+            process1.Start();
+            process2.Start();
+
+            process1.Join();
+            process2.Join();
+        }
+
+        static void DoubleCheckLock()
+        {
+            LoadBalancer b1 = LoadBalancer.GetLoadBalancer();
+            LoadBalancer b2 = LoadBalancer.GetLoadBalancer();
+            LoadBalancer b3 = LoadBalancer.GetLoadBalancer();
+            LoadBalancer b4 = LoadBalancer.GetLoadBalancer();
+
+            // Same instance?
+
+            if (b1 == b2 && b2 == b3 && b3 == b4)
+            {
+                Console.WriteLine("Same instance\n");
+            }
+
+            // Load balance 15 server requests
+
+            LoadBalancer balancer = LoadBalancer.GetLoadBalancer();
+            for (int i = 0; i < 15; i++)
+            {
+                string server = balancer.Server;
+                Console.WriteLine("Dispatch Request to: " + server);
+            }
+
+            // Wait for user
+        }
+
+
+        static void Main()
+        {
+            NaiveSingleton();
+            Console.WriteLine("----------");
+            ThreadSafeSingleton();
+            Console.WriteLine("----------");
+            DoubleCheckLock();
+            Console.ReadKey();
+        }
+    }
+
+}
